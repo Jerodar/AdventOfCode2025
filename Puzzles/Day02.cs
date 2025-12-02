43598b79ab2998b0e77bd361f62569604c5119a9
Jeroen Oostdam <5995294+Jerodar@users.noreply.github.com>
2025-12-02 06:56:44 +0100

    Day 02 part two

--- Puzzles/Day02.cs
+++ Puzzles/Day02.cs
@@ -29,5 +29,29 @@ public static class Day02
         }
         Console.WriteLine();
         Console.WriteLine(answer);
+        
+        Console.WriteLine();
+        
+        Console.WriteLine("Day 2 Part Two");
+        Console.WriteLine();
+
+        answer = 0;
+        regex = new Regex(@"^(.+)\1+$");
+        foreach (string input in inputs[0])
+        {
+            var parts = input.Split('-');
+            long first = long.Parse(parts[0]);
+            long last = long.Parse(parts[1]);
+            for (long i = first; i <= last; i++)
+            {
+                if (regex.Count(i.ToString()) > 0)
+                {
+                    Console.WriteLine($"Matched {i} of range {input}");
+                    answer += i;
+                }
+            }
+        }
+        Console.WriteLine();
+        Console.WriteLine(answer);
     }
 }
