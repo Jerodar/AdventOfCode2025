bec7ccca1ef87b87db4786c3df4805749b5f16d7
Jeroen Oostdam <5995294+Jerodar@users.noreply.github.com>
2025-12-03 07:50:17 +0100

    Cleaned up Day03 code

--- Puzzles/Day03.cs
+++ Puzzles/Day03.cs
@@ -16,10 +16,10 @@ public static class Day03
         long answer = 0;
         foreach (string input in inputs)
         {
-            int firstIndex = FindMax(0, input.Length - 1, input);
-            int secondIndex = FindMax(firstIndex + 1, input.Length, input);
+            int firstIndex = FindFirstIndexOfMax(0, input.Length - 1, input);
+            int secondIndex = FindFirstIndexOfMax(firstIndex + 1, input.Length, input);
             
-            int result = (input[firstIndex] - 48)*10 + (input[secondIndex] - 48);
+            int result = (input[firstIndex] - '0')*10 + (input[secondIndex] - '0');
             Console.WriteLine(result);
             answer += result;
         }
@@ -39,8 +39,11 @@ public static class Day03
             for (int digit = 12; digit > 0; digit--)
             {
                 int endIndex = input.Length - digit + 1;
-                startIndex = FindMax(startIndex + 1, endIndex, input);
-                result += (long)((input[startIndex] - 48) * Math.Pow(10.0,digit - 1.0));
+                startIndex = FindFirstIndexOfMax(startIndex + 1, endIndex, input);
+                
+                int number = input[startIndex] - '0';
+                long factor = (long)Math.Pow(10.0, digit - 1.0); 
+                result += number * factor;
             }
 
             Console.WriteLine(result);
@@ -50,30 +53,21 @@ public static class Day03
         Console.WriteLine(answer);
     }
 
-    private static int FindMax(int start, int end, string input)
+    private static int FindFirstIndexOfMax(int start, int end, string input)
     {
-        int[] firstIndex = Enumerable.Repeat(-1, 10).ToArray();
+        int highestNumber = 0;
+        int highestNumberIndex = 0;
         for (int i = start; i < end; i++)
         {
-            if (firstIndex[input[i] - 48] < 0)
-            {
-                firstIndex[input[i] - 48] = i;
-                if (input[i] - 48 == 9)
-                {
-                    break;
-                }
-            }
+            int number = input[i] - '0';
+            if (number <= highestNumber) continue;
+            
+            highestNumber = number;
+            highestNumberIndex = i;
+            
+            if (number == 9) break;
         }
 
-        int firstMax = 9;
-        for (; firstMax >= 0; firstMax--)
-        {
-            if (firstIndex[firstMax] >= 0)
-            {
-                break;
-            }
-        }
-
-        return firstIndex[firstMax];
+        return highestNumberIndex;
     }
 }
