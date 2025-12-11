6bf284727cdf64baa129a37e0fae2c4f1a148804
Jeroen Oostdam <5995294+Jerodar@users.noreply.github.com>
2025-12-11 08:27:14 +0100

    First sort nodelist topologically, and abort when going beyond fft or dac. Still takes too long to get the answer.

--- Puzzles/Day11.cs
+++ Puzzles/Day11.cs
@@ -122,28 +122,37 @@ public static class Day11
             }
         }
         
-        answer = CountAllPathsPassingTargets(nodes["svr"],"out");
+        var sortedNodes = TopologicalSort(nodes.Values.ToList());
+        answer = CountAllPathsPassingTargets(nodes["svr"],"out", sortedNodes);
 
         Debug.WriteLine("");
 
         return answer;
     }
     
-    private static long CountAllPathsPassingTargets(Node start, string goal)
+    private static long CountAllPathsPassingTargets(Node start, string goal, List<Node> topoSorted)
     {
         long paths = 0;
         Queue<Node> queue = [];
-    
+        
+        var dacIndex = topoSorted.FindIndex(n => n.Id == "dac");
+        var fftIndex = topoSorted.FindIndex(n => n.Id == "fft");
+        
         queue.Enqueue(start);
     
         while (queue.Count > 0)
         {
             Node node = queue.Dequeue();
             
-            if (node.Id == "fft") 
-                node.VisitedFft = true;
-            else if (node.Id == "dac") 
+            if (node.Id == "dac") 
                 node.VisitedDac = true;
+            else if (node.Id == "fft") 
+                node.VisitedFft = true;
+            
+            // Check in the topological sorted list if the node is past any of the intermediate goals
+            var currentIndex = topoSorted.FindIndex(n => n.Id == node.Id);
+            if (!node.VisitedDac && currentIndex > dacIndex) continue;
+            if (!node.VisitedFft && currentIndex > fftIndex) continue;
 
             foreach (var neighbour in node.Neighbours)
             {
@@ -165,5 +174,36 @@ public static class Day11
         }
 
         return paths;
+    }
+
+    private static List<Node> TopologicalSort(List<Node> nodes)
+    {
+        List<Node> results = [];
+        
+        Dictionary<string,int> inDegree = new();
+        Queue<Node> queue = [];
+
+        foreach (var node in nodes)
+            foreach (var neighbour in node.Neighbours)
+                if (!inDegree.TryAdd(neighbour.Id, 1))
+                    inDegree[neighbour.Id]++;
+        
+        foreach (var node in nodes)
+            if (!inDegree.ContainsKey(node.Id))
+                queue.Enqueue(node);
+
+        while (queue.Count > 0)
+        {
+            Node node = queue.Dequeue();
+            results.Add(node);
+            foreach (var neighbour in node.Neighbours)
+            {
+                inDegree[neighbour.Id]--;
+                if (inDegree[neighbour.Id] == 0)
+                    queue.Enqueue(neighbour);
+            }
+        }
+        
+        return results;
     }
 }
